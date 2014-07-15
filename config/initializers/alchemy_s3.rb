d35b84ae370e2f17a63437bde9ba6affe48ef7c3
Tom Zeng <tom@intridea.com>
2014-07-15 02:03:18 -0400

    Disable S3 storage

--- config/initializers/alchemy_s3.rb
+++ config/initializers/alchemy_s3.rb
@@ -1,5 +1,5 @@
 require 'dragonfly/s3_data_store'
-
+=begin
 pictures_app = Dragonfly.app(:alchemy_pictures)
 pictures_app.configure_with(:imagemagick)
 pictures_app.configure do
@@ -9,7 +9,7 @@ pictures_app.configure do
     secret_access_key: ENV['S3_SECRET'],
     region: ENV['S3_REGION'] || 'us-east-1',
     storage_headers: {'x-amz-acl' => 'public-read'},
-    url_scheme: 'http'
+    url_scheme: 'https'
 end
 
 attachments_app = Dragonfly.app(:alchemy_attachments)
@@ -20,5 +20,6 @@ attachments_app.configure do
     secret_access_key: ENV['S3_SECRET'],
     region: ENV['S3_REGION'] || 'us-east-1',
     storage_headers: {'x-amz-acl' => 'public-read'},
-    url_scheme: 'http'
+    url_scheme: 'https'
 end
+=end
